22cd60167ba7d8d29018ed8495f845df26987a27
Afnan_plk <pallikkelafnan@gmail.com>
2021-09-09 09:58:18 +0530

    bug fixing

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -883,7 +883,7 @@ else if (config.WORKTYPE == 'public') {
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, mimetype: Mimetype.mp4});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4});
         });
     }));
 

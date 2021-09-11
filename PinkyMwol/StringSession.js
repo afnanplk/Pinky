eb23c00de5157ca287299b470b88b00e1abaf4c4
shahid-devil <89721494+shahid-devil@users.noreply.github.com>
2021-09-11 11:32:46 +0530

    Update StringSession.js

--- PinkyMwol/StringSession.js
+++ PinkyMwol/StringSession.js
@@ -26,7 +26,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Pinky___' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'luttapi___' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 

9fa8dceb96fcaf1c5d8153b273032a4bb9c54024
shahid-devil <89721494+shahid-devil@users.noreply.github.com>
2021-09-12 16:14:37 +0530

    Create rest warn.js

---
+++ plugins/rest warn.js
@@ -0,0 +1,61 @@
+const Shazz = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const GM = "it sends good morning message"
+const GN = "it sends good night message"
+const Config = require('../config');
+
+
+
+if (Config.WORKTYPE == 'private') {
+Shazz.addCommand({pattern: 'reset warn', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[1] = "```No of warnings : 0```";
+    r_text[2] ="```No of warnings : 0```";
+   
+    
+    var i = Math.floor(3*Math.random())
+
+    await message.client.sendMessage(
+        message.jid,(r_text[i]), MessageType.text);
+
+    }));
+
+
+Shazz.addCommand({pattern: 'reset warn', fromMe: true, deleteCommand: true, desc: GN,}, (async (message, match) => {
+
+        var r_text = new Array ();
+    
+        r_text[1] = "```No of warnings : 0```";
+        r_text[2] ="```No of warnings : 0```";
+        
+      
+    
+        var i = Math.floor(3*Math.random())
+    
+        await message.client.sendMessage(
+            message.jid,(r_text[i]), MessageType.text);
+    
+        }));    
+
+    }
+    
+
+    if (Config.WORKTYPE == 'public') {
+Shazz.addCommand({pattern: 'reset warn', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
+
+    var r_text = new Array ();
+
+    r_text[1] = "```No of warnings : 0```";
+    r_text[2] ="```No of warnings : 0```";
+   
+    
+    var i = Math.floor(3*Math.random())
+
+    await message.client.sendMessage(
+        message.jid,(r_text[i]), MessageType.text);
+
+    }));    
+                
+}

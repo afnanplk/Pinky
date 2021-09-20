e19fb760200b90bfa4d1e9f2ef17f817b8206811
shahid-devil <89721494+shahid-devil@users.noreply.github.com>
2021-09-20 11:29:00 +0530

    Update scrapers3.js

--- plugins/scrapers3.js
+++ plugins/scrapers3.js
@@ -241,6 +241,25 @@ Asena.addCommand(
 );
 
 Asena.addCommand(
+  { pattern: "find", fromMe: true, desc: Lang.FIND_DESC },
+  async (message, match) => {
+    if (
+      !message.reply_message ||
+      (!message.reply_message.audio && !message.reply_message.video)
+    )
+      return await message.sendMessage(Lang.FIND_NEED_REPLY);
+    let location = await message.reply_message.downloadAndSaveMediaMessage(
+      "find"
+    );
+    let buff = await audioCut(location, 0, 15, "findo");
+    const data = await IdentifySong(buff);
+    if (!data) return;
+    if (!data.status) return await message.sendMessage(Lang.NOT_FOUND);
+    return await message.sendMessage(Lang.FIND_MSG.format(data.data.title, data.data.artists, data.data.genre, data.data.album, data.data.release_date), { quoted: message.quoted });
+  }
+);
+
+Asena.addCommand(
   { pattern: "wiki ?(.*)", fromMe: true, desc: Lang.WIKI_DESC },
   async (message, match) => {
     if (match === "") return await message.sendMessage(Lang.NEED_WORDS);

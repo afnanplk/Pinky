1d271995f395f45c1bdd0467f5414bb8cfba2211
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-10-18 13:36:05 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -3,6 +3,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 Developer & Co-Founder - Phaticusthiccy
+re-edited by afnan plk
 */
 
 const MyPnky = require('../events');

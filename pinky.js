a7e3af0631adeaa812577f6e9bb274b3075a5e53
Afnanplk <pallikkelafnan@gmail.com>
2021-10-20 08:01:27 +0530

    solved some bugs

--- pinky.js
+++ pinky.js
@@ -14,6 +14,6 @@ module.exports = {
  REMOVE2: process.env.THERI_KICK_PM === undefined ? 'false' : process.env.THERI_KICK_PM,
  PLKAFN: process.env.THERI_LIST_PM === undefined ? false : process.env.THERI_LIST_PM,
  PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
-    
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
 
 };

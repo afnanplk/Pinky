2cc5c5e9e521c021295c53856c59d1c1a4f6177e
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2022-03-22 09:13:05 +0530

    BUG FIXING

--- plugins/Talking Pinky.js
+++ plugins/Talking Pinky.js
@@ -1,1 +1,237 @@
+/* Codded by Phaticusthiccy
+eva artificial intelligence was codded by Phaticusthiccy
+also 90% of thise code is done by Phaticusthiccy
 
+re-coded or modified by afnanplk
+to bring new ai
+*/
+
+
+const MyPnky = require('../events');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const https = require('https');
+const googleTTS = require('google-translate-tts');
+const { MessageType, Mimetype, MessageOptions } = require('@adiwajshing/baileys');
+const Language = require('../language');
+const Lang = Language.getString('voicy');
+const { AfnanPLK, addplk } = require("./sql/tryplk");
+const conf = require('../config');
+const axios = require('axios')
+const axiosdef = require("axios").default;
+const os = require('os')
+const translatte = require('translatte');
+const LanguageDetect = require('languagedetect');
+const lngDetector = new LanguageDetect();
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: conf.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + conf.HEROKU.APP_NAME;
+
+let wk = conf.WORKTYPE == 'public' ? false : true
+var vtalk_dsc = ''
+var reply_eva = ''
+if (conf.LANG == 'TR') vtalk_dsc = 'Eva sesli sohbetini başlatır.', reply_eva = '*Herhangi Bir Sesli Mesaja Yanıt Verin!*'
+if (conf.LANG == 'EN') vtalk_dsc = 'Starts to pinky voice chat.', reply_eva = '*Reply to Any Voice Message!*'
+if (conf.LANG == 'AZ') vtalk_dsc = 'Eva səsli söhbətinə başlayır.', reply_eva = '*Hər hansı bir səsli mesaja cavab verin!*'
+if (conf.LANG == 'PT') vtalk_dsc = 'Começa o bate-papo por voz de Eva.', reply_eva = '*Responder a qualquer mensagem de voz!*'
+if (conf.LANG == 'RU') vtalk_dsc = 'Запускает голосовой чат Eva.', reply_eva = '*Ответьте на любое голосовое сообщение!*'
+if (conf.LANG == 'HI') vtalk_dsc = 'Eva ध्वनि चैट प्रारंभ करता है', reply_eva = '*किसी भी ध्वनि संदेश का उत्तर दें!*'
+if (conf.LANG == 'ES') vtalk_dsc = 'Comienza con el chat de voz de Eva.', reply_eva = '*¡Responde a cualquier mensaje de voz!*'
+if (conf.LANG == 'ML') vtalk_dsc = 'വോയ്‌സ് ചാറ്റിലേക്ക് ആരംഭിക്കുന്നു.', reply_eva = '*ഏത് വോയ്‌സ് സന്ദേശത്തിനും മറുപടി നൽകുക!*'
+if (conf.LANG == 'ID') vtalk_dsc = 'Mulai obrolan suara Eva.', reply_eva = '*Balas Pesan Suara Apapun!*'
+
+const recognizeAudio = () => {
+    const headers = new Headers({
+        'Content-Type': 'audio/wav',
+        "Authorization": `Bearer ${conf.WITAI_API}`,
+        'Cache-Control': 'no-cache',
+        'Transfer-Encoding': 'chunked'
+    })
+    const requestBody = {
+        method: "POST",
+        body: fs.readFileSync('output.wav'),
+        headers: headers
+    }
+    return fetch("https://api.wit.ai/speech?v=20200219", requestBody)
+        .then(response => response.json())
+        .then(json => json._text)
+}
+const convertToWav = file => {
+    return ffmpeg(file)
+        .audioCodec('pcm_s16le')
+        .format('wav')
+        .save('output.wav')
+}
+if (conf.STANDPLK == 'off' || conf.STANDPLK == 'OFF') {
+MyPnky.addCommand({on: 'text', fromMe: wk, dontAddCommandList: true, deleteCommand: false}, (async (message, match) => {
+	var plkser = await addplk(message.jid);
+    if (message.message.startsWith('pinky') && !plkser.message.includes('{pinky}')) {        
+        var unique_ident = message.client.user.jid.split('@')[0]      
+        let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
+        let aitalk_mode = message.message.includes('{normal}') ? 'raw' : 'waifu'
+        var finm = message.message.replace('pinky', '').replace(' ', '')   
+        var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
+        if (ainame !== 'Asena') return;
+        var ldet = lngDetector.detect(finm)
+        var trmsg = ''
+        if (ldet[0][0] !== 'english') {
+            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+            if ('text' in ceviri) {
+                trmsg = ceviri.text
+            }
+        } else { trmsg = finm }
+        var uren = encodeURI(trmsg)
+        await axios.get('http://api.brainshop.ai/get?bid=161217&key=DkuVhwkGrxBLnsOi&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+            var fins = ''                           
+            if (conf.LANG !== 'EN') {
+                ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                if ('text' in ceviri) {
+                    fins = ceviri.text
+                }
+            } else { fins = response.data.cnt }
+            await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+        })
+    }
+}));
+MyPnky.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+	var plkser = await addplk(message.jid);
+        if (plkser.message.includes('{pinky}') && ((!message.jid.includes('g.us')) || (message.jid.includes('g.us') && 
+            (( message.mention !== false && message.mention.length !== 0 ) || message.reply_message !== false)))) {
+            if (message.jid.includes('g.us') && (message.mention !== false && message.mention.length !== 0)) {
+                message.mention.map(async (jid) => {
+                    if (message.client.user.jid.split('@')[0] === jid.split('@')[0]) {
+                        var unique_ident = message.client.user.jid.split('@')[0]      
+                        let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
+                        let aitalk_mode = message.message.includes('{normal}') ? 'raw' : 'waifu'                       
+                        var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
+                        if (ainame !== 'Asena') return;
+                        var finm = message.message
+                        var ldet = lngDetector.detect(finm)
+                        var trmsg = ''
+                        if (ldet[0][0] !== 'english') {
+                            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                            if ('text' in ceviri) {
+                                trmsg = ceviri.text
+                            }
+                        } else { trmsg = finm }
+                        var uren = encodeURI(trmsg)
+                        await axios.get('http://api.brainshop.ai/get?bid=161217&key=DkuVhwkGrxBLnsOi&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+                            var fins = ''                           
+                            if (conf.LANG !== 'EN') {
+                                ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                                if ('text' in ceviri) {
+                                    fins = ceviri.text
+                                }
+                            } else { fins = response.data.cnt }
+                            await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+                        })
+                    }
+                })
+            } else if (message.jid.includes('g.us') && message.reply_message !== false) {
+                if (message.reply_message.jid.split('@')[0] === message.client.user.jid.split('@')[0]) {
+                    var unique_ident = message.client.user.jid.split('@')[0]      
+                    let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
+                    var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
+                    if (ainame !== 'Asena') return;
+                    var finm = message.message
+                    var ldet = lngDetector.detect(finm)
+                    var trmsg = ''
+                    if (ldet[0][0] !== 'english') {
+                        ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                        if ('text' in ceviri) {
+                            trmsg = ceviri.text
+                        }
+                    } else { trmsg = finm }
+                    var uren = encodeURI(trmsg)
+                    await axios.get('http://api.brainshop.ai/get?bid=161217&key=DkuVhwkGrxBLnsOi&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+                        var fins = ''                           
+                        if (conf.LANG !== 'EN') {
+                            ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                            if ('text' in ceviri) {
+                                fins = ceviri.text
+                            }
+                        } else { fins = response.data.cnt }
+                        await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+                    })
+                }
+            } else {
+                var unique_ident = message.client.user.jid.split('@')[0]      
+                let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
+                var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
+                if (ainame !== 'Asena') return;
+                var finm = message.message
+                var ldet = lngDetector.detect(finm)
+                var trmsg = ''
+                if (ldet[0][0] !== 'english') {
+                    ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                    if ('text' in ceviri) {
+                        trmsg = ceviri.text
+                    }
+                } else { trmsg = finm }
+                var uren = encodeURI(trmsg)
+                await axios.get('http://api.brainshop.ai/get?bid=161217&key=DkuVhwkGrxBLnsOi&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
+                    var fins = ''                           
+                    if (conf.LANG !== 'EN') {
+                        ceviri = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                        if ('text' in ceviri) {
+                            fins = ceviri.text
+                        }
+                    } else { fins = response.data.cnt }
+                    await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
+                })
+            }
+        }
+
+}));
+MyPnky.addCommand({ pattern: 'vtalk$', desc: vtalk_dsc,dontAddCommandList: true, fromMe: wk }, (async (message, match) => {
+    if (!message.reply_message) return await message.client.sendMessage(message.jid,reply_eva, MessageType.text, { quoted: message.data }) 
+    try {
+        const file = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        })
+        
+        convertToWav(file)
+            .on('end', async () => {
+                const recognizedText = await recognizeAudio()
+                
+                var ssc = ''
+                ceviri = await translatte(recognizedText, {from: 'auto', to: 'EN' });
+                if ('text' in ceviri) {
+                    ssc = ceviri.text
+                }
+                var unique_ident = message.client.user.jid.split('@')[0]
+                let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'       
+                var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
+                if (ainame !== 'Asena') return;
+        
+                var son = encodeURI(ssc)
+                await axios.get('http://api.brainshop.ai/get?bid=161217&key=DkuVhwkGrxBLnsOi&uid=' + unique_ident + '&msg=' + son).then(async (response) => {
+                    var trmsg = ''
+                    cevir = await translatte(response.data.cnt, {from: 'auto', to: conf.LANG});
+                    if ('text' in cevir) {
+                        trmsg = cevir.text
+                    }
+            
+                    let 
+                        LANG = conf.LANG.toLowerCase(),
+                        ttsMessage = trmsg,
+                        SPEED = 1.0
+                    var buffer = await googleTTS.synthesize({
+                        text: ttsMessage,
+                        voice: LANG
+                    });
+            
+                    await message.client.sendMessage(message.jid,buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true, quoted: message.data})
+                }).catch(async (error) => {
+	            console.log(error)
+                });
+        });
+    } catch (err) { console.log(err) }
+}));
+}
